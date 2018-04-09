7657fe9f048758b854a667ff34b14bb6aa2e30cc
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2018-04-10 00:37:10 +0530

    Atlast

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 var connectPath, options;
 //Check if we are on Heroku
 if (process.env.PORT) {
-  connectPath = "mongodb://ankitbookbash>@ds241039.mlab.com:41039/bookbash";
+  connectPath = "mongodb://ankitbookbash@ds241039.mlab.com:41039/bookbash";
   options = {
     auth: {
       user: "ankitbookbash",
