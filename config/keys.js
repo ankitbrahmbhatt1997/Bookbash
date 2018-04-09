9ef1ec0896ddf9169a8fe343ebc2407c1366c328
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2018-04-09 23:43:06 +0530

    new 4

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,9 @@
 // if (process.env.NODE_ENV === "production") {
 //   module.exports = require("./keys_prod");
 // } else {
-module.exports = require("./keys_dev");
+// module.exports = require("./keys_dev");
+module.exports = {
+  google_client_id:
+    "351522286223-sn0ieh5uun3dtt7ancg2n2qrb51vs21m.apps.googleusercontent.com",
+  google_client_secret: "6Bt6XvW_Yb5qFboCoGBIWEFb"
+};
